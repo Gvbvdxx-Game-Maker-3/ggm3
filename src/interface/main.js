150ba7077f83faca801ddd14c749eeefcbc6ba99
gvbvdxx <123641869+gvbvdxx@users.noreply.github.com>
2025-11-18 16:58:15 +0000

    Commit

--- src/interface/main.js
+++ src/interface/main.js
@@ -2,4 +2,4 @@ require("./dom/index.js");
 
 var elements = require("../gp2/elements.js");
 var AElement = require("../gp2/aelement.js");
-var blocks = require("./blocks.js");
+//var { createFreshWorkspace, getCurrentWorkspace } = require("./blocks.js");
