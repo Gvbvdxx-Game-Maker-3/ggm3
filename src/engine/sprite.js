7b672415206149930383586b79bcc36a83803dc3
Gvbvdxx (Alt Account) <jgevans@students.wcpss.net>
2026-02-04 18:03:47 +0000

    Remove window.alert

--- src/engine/sprite.js
+++ src/engine/sprite.js
@@ -87,13 +87,9 @@ class Sprite {
   setSProperty(spriteName, name, value) {
     var targetSprite = this.findSpriteByName(spriteName);
     if (!targetSprite) {
-      window.alert("target sprite not found");
       return;
     }
     targetSprite.spriteProperties[name] = value;
-        window.alert(name);
-    window.alert(value);
-    window.alert(targetSprite.spriteProperties[name]);
   }
 
   changeSProperty(spriteName, name, value) {
@@ -102,14 +98,10 @@ class Sprite {
       return;
     }
     targetSprite.spriteProperties[name] = (+targetSprite.spriteProperties[name] || 0) + (+value || 0);
-    window.alert(name);
-    window.alert(value);
-    window.alert(targetSprite.spriteProperties[name]);
   }
 
   getSProperty(spriteName, name) {
     var targetSprite = this.findSpriteByName(spriteName);
-    window.alert(`Looking for "${spriteName}": Found?: ${targetSprite}`);
     if (!targetSprite) {
       return;
     }
