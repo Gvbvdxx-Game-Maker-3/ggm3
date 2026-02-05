b2ae64a24aecf318d99337655c5cce73e16edd94
Gvbvdxx (Alt Account) <jgevans@students.wcpss.net>
2026-02-05 17:04:55 +0000

    Commit

--- src/interface/file/ggm3zip.js
+++ src/interface/file/ggm3zip.js
@@ -128,7 +128,11 @@ async function loadProjectZip(zipSource, progress = new ProgressMonitor()) {
     for (var spriteJson of engineJson.sprites) {
         var sprite = engine.createEmptySprite();
         for (var costumeJson of spriteJson.costumes) {
-            var 
+            
+
+            var costume = await loadCostume(
+                
+            );
         }
     }
 }
