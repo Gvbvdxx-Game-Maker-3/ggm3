646a308011221bd2f56b68db62a07f07b830d8a9
Jason Glenn Evans <jgevans@students.wcpss.net>
2026-08-18 17:51:28 +0000

    Commit

--- src/interface/file/exported-game-base/game.base.js
+++ src/interface/file/exported-game-base/game.base.js
@@ -85,66 +85,6 @@
 
   //Most of these used from from-to.js
 
-  function _fromEnginePropertyNames(from) {
-    engine.propertyVariables = {};
-    for (var name of from || []) {
-      engine.propertyVariables[name] = true;
-    }
-  }
-  function fromEngineJSON(mainJSON) {
-    Object.assign(engine, {
-      globalVariables: mainJSON.globalVariables || {},
-      broadcastNames: mainJSON.broadcastNames || [],
-      frameRate: mainJSON.frameRate || 60,
-      gameWidth: mainJSON.gameWidth || engine.DEFAULT_WIDTH,
-      gameHeight: mainJSON.gameHeight || engine.DEFAULT_HEIGHT,
-    });
-    _fromEnginePropertyNames(mainJSON.spriteProperties);
-    engine.updateCanvasSize();
-  }
-
-  function fromSpriteJSON(sprite, spriteJson) {
-    Object.assign(sprite, {
-      x: spriteJson.x,
-      y: spriteJson.y,
-      angle: spriteJson.angle,
-      scaleX: spriteJson.scaleX,
-      scaleY: spriteJson.scaleY,
-      skewX: spriteJson.skewX || 0,
-      skewY: spriteJson.skewY || 0,
-      size: spriteJson.size,
-      //blocklyXML isn't used in exports since we're using pre-generated code, and also because there is no Blockly library included.
-      //blocklyXML: spriteJson.blocklyXML
-      //  ? Blockly.Xml.textToDom(spriteJson.blocklyXML)
-      //  : null,
-      name: spriteJson.name,
-      costumeIndex: spriteJson.costumeIndex,
-      zIndex: spriteJson.zIndex,
-      variables: spriteJson.variables,
-      hidden: spriteJson.hidden,
-      spriteProperties: spriteJson.properties || {},
-    });
-  }
-
-  function fromCostumeJSON(costume, costumeJson) {
-    Object.assign(costume, {
-      id: costumeJson.id,
-      rotationCenterX: costumeJson.rotationCenterX,
-      rotationCenterY: costumeJson.rotationCenterY,
-      preferedScale: costumeJson.preferedScale,
-      willPreload: costumeJson.willPreload,
-      mimeType: costumeJson.mimeType,
-    });
-  }
-
-  function fromSoundJSON(sound, soundJson) {
-    Object.assign(sound, {
-      id: soundJson.id,
-      willPreload: soundJson.willPreload,
-      mimeType: soundJson.mimeType,
-    });
-  }
-
   //////////////////////////////////////////////////////////////////
 
   //Function used to assign the generated sprite
