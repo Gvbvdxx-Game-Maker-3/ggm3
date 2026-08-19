c7a8805ec43d38decf03da06abcf743aee4f224f
Jason Glenn Evans <jgevans@students.wcpss.net>
2026-08-19 18:14:14 +0000

    Commit

---
+++ src/interface/gameinfo.js
@@ -0,0 +1,9 @@
+class GameInfo {
+    constructor () {
+        this.name = "GGM3 Project";
+        this.description = "Your very own game written with GGM3!";
+        this.icon = null;
+    }
+
+
+}
